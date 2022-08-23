21149a0a5862fb82fdafccc74e67a47cc52a1863
shwangson <74443185+shwangson@users.noreply.github.com>
2022-08-24 00:41:56 +0800

    Update TcpSocketPackage.java

--- android/src/main/java/com/asterinet/react/tcpsocket/TcpSocketPackage.java
+++ android/src/main/java/com/asterinet/react/tcpsocket/TcpSocketPackage.java
@@ -5,22 +5,28 @@ import java.util.Collections;
 import java.util.List;
 
 import com.facebook.react.ReactPackage;
+import com.facebook.react.bridge.JavaScriptModule;
 import com.facebook.react.bridge.NativeModule;
 import com.facebook.react.bridge.ReactApplicationContext;
 import com.facebook.react.uimanager.ViewManager;
 
-import androidx.annotation.NonNull;
+//import androidx.annotation.NonNull;
 
 @SuppressWarnings("unused")
 public class TcpSocketPackage implements ReactPackage {
     @Override
-    public @NonNull List<NativeModule> createNativeModules(@NonNull ReactApplicationContext reactContext) {
+    public List<NativeModule> createNativeModules(ReactApplicationContext reactContext) {
         //noinspection ArraysAsListWithZeroOrOneArgument
         return Arrays.<NativeModule>asList(new TcpSocketModule(reactContext));
     }
 
     @Override
-    public @NonNull List<ViewManager> createViewManagers(@NonNull ReactApplicationContext reactContext) {
+    public List<Class<? extends JavaScriptModule>> createJSModules() {
+        return null;
+    }
+
+    @Override
+    public List<ViewManager> createViewManagers( ReactApplicationContext reactContext) {
         return Collections.emptyList();
     }
 }
